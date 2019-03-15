6bf6ee4036c7224d6d422d7bc86274c3c0f56303
1712056 <1712056@student.hcmus.edu.vn>
2019-03-15 12:39:20 +0700

    Cong chuoi

--- BigInt/QInt.cpp
+++ BigInt/QInt.cpp
@@ -21,6 +21,7 @@ string chia2(string s)
 			{
 				i++;
 				divisor = (divisor * 10) + (s[i] - 48);
+		
 			}
 			char c = (divisor / 2) + 48;
 			temp += c;
@@ -32,7 +33,6 @@ string chia2(string s)
 	return s;
 
 }
-
 
 QInt::QInt()
 {
@@ -52,93 +52,30 @@ void QInt::Nhap()
 
 	//Tiến hành đổi s sang dạng nhị phân lưu vào mảng a
 	int i = 0;
-	if (s[0] != '-' || s[0]=='+') //Số nhập vào là số dương
+	while (s != "0")
 	{
-		while (s != "0")
+		//Xét số cuối cùng của chuỗi, nếu là số chẵn khi chia 2 sẽ dư 0 
+		if ((s[s.length() - 1] - 48) % 2 == 0)  
 		{
-			//Xét số cuối cùng của chuỗi, nếu là số chẵn khi chia 2 sẽ dư 0 
-			if ((s[s.length() - 1] - 48) % 2 == 0)
-			{
-				a[i++] = 0;
-			}
-			//Nếu là số lẻ, chia 2 sẽ dư 1
-			else
-			{
-				a[i++] = 1;
-			}
-
-			//Chia chuỗi s cho 2
-			s = chia2(s);
-			if (i > 126)
-			{
-				throw "Stack Overflow";
-			}
-
-		};
-		a[127] = 0; //Bit dấu
-	}
-
-	else //Số nhập vào là số âm
-	{
-		string positive_s;
-		for (int i = 1; i < s.length(); i++) //Lấy phần sau dấu âm của số
-		{
-			positive_s += s[i];
+			a[i++] = 0;
 		}
-		while (positive_s != "0")
+		//Nếu là số âm, chia 2 sẽ dư 1
+		else
 		{
-			//Xét số cuối cùng của chuỗi, nếu là số chẵn khi chia 2 sẽ dư 0 
-			if ((positive_s[positive_s.length() - 1] - 48) % 2 == 0)
-			{
-				a[i++] = 0;
-			}
-			//Nếu là số lẻ, chia 2 sẽ dư 1
-			else
-			{
-				a[i++] = 1;
-			}
-
-			//Chia chuỗi s cho 2
-			positive_s = chia2(positive_s);
-			if (i > 126)
-			{
-				throw "Stack Overflow";
-			}
-
-		};
-		a[127] = 1; //Bit dấu
-
-		// Đảo bit thành dạng bù 1
-		for (int i = 0; i < 127; i++)
-		{
-			if (a[i] == 1)
-			{
-				a[i] = 0;
-			}
-			else
-			{
-				a[i] = 1;
-			}
+			a[i++] = 1;
 		}
 
-		//Cộng 1 vào kết quả thành dạng bù 2
-		for (int i = 0; i < 127; i++)
-		{
-			if (a[i] == 1)
-			{
-				a[i] = 0;
-			}
-			else
-			{
-				a[i] = 1;
-				break;
-			}
-		}
-	}
+		//Chia chuỗi s cho 2
+		s = chia2(s);
+
+	};
+
 
 	// Bật các bit của data bằng cách OR với giá trị của mảng a tương ứng
 	int count = 0;
 	int d = 0;
+	int temp = 0;
+	
 	for (int i = 127; i >= 0; i--)
 	{
 		data[d] = data[d] | (a[i] << (32 - 1 - count));
@@ -149,6 +86,66 @@ void QInt::Nhap()
 			d++;
 		}
 	}
+	
+}
+string PlusBigInt(string a, string b)
+{
+	if (a.length() < b.length())
+	{
+		string s = a;
+		a = b;
+		b = s;
+	}
+	string result;
+	for (int i = 0; i < a.length() + 1; i++)
+	{
+		result += "0";
+	}
+	bool remember = false;
+	for (int i = 0; i < b.length(); i++)
+	{
+		int temp = b[b.length() - i - 1] - '0' + a[a.length() - i - 1] - '0';
+
+		if (remember)
+			temp++;
+
+		if (temp > 9)
+			remember = true;
+		else
+			remember = false;
+
+		temp = temp % 10;
+
+		result[a.length() - i] = temp + '0';
+	}
+	for (int i = 0; i < a.length() - b.length(); i++)
+	{
+		int temp = a[a.length() - b.length() - i - 1] - '0';
+
+		if (remember)
+			temp++;
+		if (temp > 9)
+			remember = true;
+		else
+			remember = false;
+
+		temp = temp % 10;
+
+		result[a.length() - b.length() - i] = temp + '0';
+	}
+
+	result[a.length() + 1] = '\0';
+
+	if (remember)
+	{
+		result[0] = '1';
+	}
+	else
+	{
+		for (int i = 0; i <= a.length(); i++)
+			result[i] = result[i + 1];
+	}
+	return result;
 }
 
 void QInt::Xuat()
@@ -156,8 +153,8 @@ void QInt::Xuat()
 	for (int i = 0; i < 4; i++)
 	{
 		cout << data[i];
+		
 	}
-
 }
 
 QInt::~QInt()
--- BigInt/QInt.h
+++ BigInt/QInt.h
@@ -17,3 +17,4 @@ public:
 };
 
 string chia2(string s);
+
