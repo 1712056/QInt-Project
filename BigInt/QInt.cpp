830dda68a86575c6042d72cfc3a21125c69a65d9
pduy99 <duylove99@gmail.com>
2019-03-16 13:08:50 +0700

    Can output a negative number

--- BigInt/QInt.cpp
+++ BigInt/QInt.cpp
@@ -34,6 +34,58 @@ string chia2(string s)
 
 }
 
+string multiple2(string s)
+{
+	int temp = 0, carry = 0;;
+	int _length = s.length();
+	int a_size = _length + 1;
+	int *a = new int[a_size];
+	string result;
+	for (int i = 0; i < a_size; i++)
+		a[i] = 0;
+	for (int i = _length - 1; i >= 0; i--)
+	{
+		temp = (s[i] - '0') * 2;
+		if (carry == 1)
+			temp++;
+		if (temp > 9)
+			carry = 1;
+		else
+			carry = 0;
+		a[i + 1] += temp % 10;
+
+	}
+	if (carry == 1)
+	{
+		a[0] = 1;
+	}
+	else
+	{
+		for (int i = 0; i < a_size - 1; i++)
+			a[i] = a[i + 1];
+		a_size--;
+	}
+	for (int i = 0; i < a_size; i++)
+	{
+		result += (a[i] + '0');
+	}
+	return result;
+}
+
+string myPow(int n) //n là số mũ
+{
+	string temp = "2";
+	if (n == 0)
+		return "1";
+	if (n == 1)
+		return "2";
+	for (int i = 0; i < n - 1; i++)
+	{
+		temp = multiple2(temp);
+	}
+	return temp;
+}
+
 QInt::QInt()
 {
 	data[0] = 0;
@@ -150,14 +202,16 @@ void QInt::Nhap()
 		}
 	}
 }
+
+//Hàm cộng 2 số dạng chuỗi
 string plusNumInStr(string a, string b)
 {
-	string result;
+	string result; //biến chứa kết quả
 
-	int i = a.length() - 1;
+	int i = a.length() - 1; 
 	int j = b.length() - 1;
 	int nho = 0;
-	while (i >= 0 && j >= 0)
+	while (i >= 0 && j >= 0) //Vòng lặp cộng các chữ số ở cuối của a, b 
 	{
 		int temp = (a[i] - 48) + (b[j] - 48);
 		char c = (temp % 10) + nho + 48;
@@ -171,7 +225,8 @@ string plusNumInStr(string a, string b)
 		i--;
 		j--;
 	}
-
+	
+	// Trường hợp số a nhiều chữ số hơn
 	while (i >= 0)
 	{
 		int temp = (a[i] - 48) + nho;
@@ -186,6 +241,7 @@ string plusNumInStr(string a, string b)
 		i--;
 	}
 
+	//Trường hợp số b nhiều chữ số hơn
 	while (j >= 0)
 	{
 		int temp = (b[j] - 48) + nho;
@@ -200,18 +256,20 @@ string plusNumInStr(string a, string b)
 		j--;
 	}
 
+	// Nếu sau khi cộng xong mà vẫn còn nhớ 1 thì ghi thêm 1 vào kết quả
 	if (nho == 1)
 	{
 		result += "1";
 	}
 
-	reverse(result.begin(), result.end());
+	reverse(result.begin(), result.end()); //Vì kết quả lưu ngược nên phải đảo chuỗi cho đúng 
 	return result;
 }
 
 void QInt::Xuat()
 {
 	unsigned int a[128] = { 0 };
+	// Đọc các bit của data lưu vào mảng a, bit dấu là bit a[127]
 	int d = 3, count = 0;
 	for (int i = 0; i < 128; i++)
 	{
@@ -238,59 +296,46 @@ void QInt::Xuat()
 			}
 		}
 	}
+	else //Bit dấu = 1
+	{
+		// Trừ 1 để thành dạng bù 1
+		for (int i = 0; i < 127; i++)
+		{
+			if (a[i] == 0)
+			{
+				a[i] = 1;
+			}
+			else
+			{
+				a[i] = 0;
+				break;
+			}
+		}
+		// Đảo bit
+		for (int i = 0; i < 127; i++)
+		{
+			if (a[i] == 1)
+				a[i] = 0;
+			else
+				a[i] = 1;
+		}
+
+		for (int i = 0; i < 127; i++)
+		{
+			if (a[i] == 1)
+			{
+				temp = myPow(i);
+				result = plusNumInStr(result, temp);
+			}
+		}
+		reverse(result.begin(), result.end()); //Đảo chuỗi
+		result += "-"; //Thêm dấu âm vào cuối chuỗi
+		reverse(result.begin(), result.end()); //Đảo chuỗi cho đúng kết quả
+	}
 	cout << result;
 }
 
-string multiple2(string s)
-{
-	int temp = 0, carry = 0;;
-	int _length = s.length();
-	int a_size = _length + 1;
-	int *a = new int[a_size];
-	string result;
-	for (int i = 0; i < a_size; i++)
-		a[i] = 0;
-	for (int i = _length - 1; i >= 0; i--)
-	{
-		temp = (s[i] - '0') * 2;
-		if (carry == 1)
-			temp++;
-		if (temp > 9)
-			carry = 1;
-		else
-			carry = 0;
-		a[i + 1] += temp % 10;
 
-	}
-	if (carry == 1)
-	{
-		a[0] = 1;
-	}
-	else
-	{
-		for (int i = 0; i < a_size - 1; i++)
-			a[i] = a[i + 1];
-		a_size--;
-	}
-	for (int i = 0; i < a_size; i++)
-	{
-		result += (a[i] + '0');
-	}
-	return result;
-}
-string myPow(int n) //n là số mũ
-{
-	string temp="2";
-	if (n == 0)
-		return "1";
-	if (n == 1)
-		return "2";
-	for (int i = 0; i < n-1; i++)
-	{
-		temp = multiple2(temp);
-	}
-	return temp;
-}
 QInt::~QInt()
 {
 }
